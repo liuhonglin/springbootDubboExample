b8010cf3b4a9e7d433961c7aba9789f06f851115
liuhonglin <liuhonglinh@163.com>
2017-06-06 23:44:38 +0800

    使用redis+lua加入限流

--- springboot-dubbo-consume/src/main/java/lhl/test/dubbo/consume/controller/ExampleController.java
+++ springboot-dubbo-consume/src/main/java/lhl/test/dubbo/consume/controller/ExampleController.java
@@ -1,10 +1,18 @@
 package lhl.test.dubbo.consume.controller;
 
 import lhl.test.dubbo.api.example.ExampleService;
+import lhl.test.dubbo.consume.utils.InternetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * Created by HL.L on 2017/6/3.
@@ -13,11 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("example")
 public class ExampleController {
 
+    private Logger logger = LoggerFactory.getLogger(ExampleController.class);
+
     @Autowired
     private ExampleService exampleService;
 
     @RequestMapping("hello")
-    public String hello(@RequestParam String name) {
-        return exampleService.hello(name);
+    public String hello(@RequestParam String name, HttpServletRequest request) {
+        String ip = InternetUtil.getIP(request);
+        logger.info(ip);
+        try {
+            if ( InternetUtil.acquire(ip, "1") ) {
+                return exampleService.hello(name);
+            } else {
+                return "limited.";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "limited.";
     }
+
+
 }
---
+++ springboot-dubbo-consume/src/main/java/lhl/test/dubbo/consume/utils/InternetUtil.java
@@ -0,0 +1,66 @@
+package lhl.test.dubbo.consume.utils;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.Files;
+import redis.clients.jedis.Jedis;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * Created by HL.L on 2017/6/5.
+ */
+public class InternetUtil {
+
+    public static String getIP(HttpServletRequest request)
+    {
+        String ip = request.getHeader("Cdn-Src-Ip");
+        if ((ip != null) && (!"".equals(ip))) {
+            return ip;
+        }
+        ip = request.getHeader("x-forwarded-for");
+        if ((ip == null) || (ip.length() == 0)) {
+            ip = request.getRemoteAddr();
+        } else {
+            String[] ips = ip.split(",");
+            if (ips != null) {
+                for (int i = 0; i < ips.length; i++) {
+                    String temp = ips[i];
+                    if ((!temp.startsWith("192.168.")) && (!temp.startsWith("10.")) && (!temp.startsWith("172."))) {
+                        return temp;
+                    }
+                }
+
+                return ips[0];
+            }
+        }
+        return ip;
+    }
+
+    /**
+     *
+     * @param ip 用户ip
+     * @param limit 限流大小
+     * @return
+     * @throws IOException
+     */
+    public static boolean acquire(String ip, String limit) throws IOException {
+        /*StringBuffer luaScript = new StringBuffer();
+        List<String> lines = Files.readAllLines(Paths.get("lua/limit.lua"), Charset.defaultCharset());
+        for(String line : lines) {
+            luaScript.append(line);
+        }*/
+        //new File(InternetUtil.class.getResource("/").getPath()+File.separator+"lua/limit.lua")
+        String luaScript = Files.toString(new File("E:\\program files\\IdeaWorkspace\\test\\springboot-dubbo-consume\\target\\classes\\lua\\limit.lua"),
+                Charset.defaultCharset());
+
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        // 此处将当前时间截取秒数
+        String key = ip + ":" + System.currentTimeMillis() / 1000;
+        return (Long)jedis.eval(luaScript.toString(), Lists.newArrayList(key),
+                            Lists.newArrayList(limit)) == 1;
+    }
+
+}
