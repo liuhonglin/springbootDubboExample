dc6d74df7dfde0dfb7b47bf787f9c039658365af
liuhonglin <liuhonglinh@163.com>
2017-06-08 17:20:02 +0800

    读取lua脚本

--- springboot-dubbo-consume/src/main/java/lhl/test/dubbo/consume/utils/InternetUtil.java
+++ springboot-dubbo-consume/src/main/java/lhl/test/dubbo/consume/utils/InternetUtil.java
@@ -5,8 +5,7 @@ import com.google.common.io.Files;
 import redis.clients.jedis.Jedis;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
 
 /**
@@ -47,15 +46,24 @@ public class InternetUtil {
      * @throws IOException
      */
     public static boolean acquire(String ip, String limit) throws IOException {
-        /*StringBuffer luaScript = new StringBuffer();
-        List<String> lines = Files.readAllLines(Paths.get("lua/limit.lua"), Charset.defaultCharset());
+        StringBuffer luaScript = new StringBuffer();
+        /*List<String> lines = Files.readAllLines(Paths.get("lua/limit.lua"), Charset.defaultCharset());
         for(String line : lines) {
             luaScript.append(line);
         }*/
         //new File(InternetUtil.class.getResource("/").getPath()+File.separator+"lua/limit.lua")
 
-        String luaScript = Files.toString(new File(InternetUtil.class.getClassLoader().getResourceAsStream("lua/limit.lua"),
-                                                    Charset.defaultCharset()););
+        BufferedInputStream bi = new BufferedInputStream(InternetUtil.class.getClassLoader().getResourceAsStream("lua/limit.lua"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(bi));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            luaScript.append(line).append("\r");
+        }
+        br.close();
+        bi.close();
+
+        //String luaScript = Files.toString(new File(InternetUtil.class.getClassLoader().getResourceAsStream("lua/limit.lua"),
+        //                                            Charset.defaultCharset()));
 
         Jedis jedis = new Jedis("127.0.0.1", 6379);
         // 此处将当前时间截取秒数
